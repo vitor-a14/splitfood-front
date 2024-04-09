8d4d3020b251bdf1a245235dba0ba7395ae4851d
TaleSun <vitoraugusto_14@outlook.com>
2024-04-09 15:18:57 -0300

    Basic interface study-case

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Alert, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const {username,setUsername}=  useState("");
+  const {password,setPassword}=  useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>SpliFood!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>SplitFood</Text>
+
+      <View style={styles.inputView}>
+        <TextInput style={styles.input} placeholder='Nome do Usuário' value={username} onChangeText={setUsername} autoCorrect={false}
+      autoCapitalize='none' />
+        <TextInput style={styles.input} placeholder='Senha' secureTextEntry value={password} onChangeText={setPassword} autoCorrect={false}
+      autoCapitalize='none'/>
+
+        <View style={styles.buttonView}>
+          <Pressable style={styles.button} onPress={() => Alert.alert("Login Successfuly!","yay")}>
+              <Text style={styles.buttonText}>Entrar</Text>
+          </Pressable>
+        </View>
+
+      </View>
+
+      <Text style={styles.footerText}>Não tem uma conta ainda?<Text style={styles.signup}> Cadastre aqui</Text></Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container : {
+    display: 'flex',
+    flexDirection: 'column',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 70,
+    alignItems: "center",
+    justifyContent : "top",
   },
+  title : {
+    fontSize : 30,
+    fontWeight : "bold",
+    textAlign: "center",
+    paddingVertical : 40,
+    color : "red"
+  },
+  inputView : {
+    gap : 15,
+    width : "100%",
+    paddingHorizontal : 40,
+    marginBottom : 5,
+    flex: 1
+  },
+  input : {
+    height : 50,
+    paddingHorizontal : 20,
+    borderColor : "red",
+    borderWidth : 1,
+    borderRadius: 7
+  },
+  button : {
+    backgroundColor : "red",
+    height : 45,
+    borderColor : "gray",
+    borderWidth  : 1,
+    borderRadius : 5,
+    alignItems : "center",
+    justifyContent : "center"
+  },
+  buttonText : {
+    color : "white"  ,
+    fontSize: 18,
+    fontWeight : "bold"
+  }, 
+  buttonView :{
+    width :"100%",
+    paddingHorizontal : 50
+  },
+  footerText : {
+    height: 100,
+    justifyContent : "top",
+    textAlign: "center",
+    color : "gray",
+  },
+  signup : {
+    color : "red",
+    fontSize : 13
+  }
 });
